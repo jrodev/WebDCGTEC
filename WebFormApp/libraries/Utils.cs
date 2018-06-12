014967a6b5257576a9bd325dd11735cfc64c671f
Jaime Rodriguez <jrodev@yahoo.es>
2018-06-12 05:46:27 -0500

    add utils and jquery

---
+++ WebFormApp/libraries/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+    public class Utils
+    {
+        static public string baseUrl()
+        {
+            return string.Format("{0}://{1}{2}",
+            HttpContext.Current.Request.Url.Scheme,
+            HttpContext.Current.Request.ServerVariables["HTTP_HOST"],
+            (HttpContext.Current.Request.ApplicationPath.Equals("/")) ? string.Empty : HttpContext.Current.Request.ApplicationPath
+            );
+        }
+    }
