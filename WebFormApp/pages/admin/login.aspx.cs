68d926c270278c68746e32547e02567fd9d05c67
jrodev <jrodev@yahoo.es>
2018-06-28 17:34:37 -0500

    update jue28jun login

--- WebFormApp/pages/admin/login.aspx.cs
+++ WebFormApp/pages/admin/login.aspx.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
